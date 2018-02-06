65909d2e7839ea3d8e1c65df99ddfff300f6e6c5
drvstkj <drvstkj@users.noreply.github.com>
2018-02-06 12:04:52 -0400

    Mark.h

---
+++ Mark.h
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Mark
+{
+public:
+Markscheme (std::string name, int studentMark)
+:mark{studentMark}
+{
+if (studentMark >=0)
+mark=studentMark;
+}
+void setname(std::string name)
+{name=name}
+std::string getname()const
+{
+return name;
+}
+void setstudentMark (std:: studentMark)
+studentMark = studentMark
+std::string getstudentMark (std:: int studentMark)
+{
+return studentMark;
+}
+};
+{
+private:
+(std::name, int mark)
+};
+
+
